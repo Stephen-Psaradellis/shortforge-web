1ddb3f97f7ffbefec72646c8f8515bb509d4767a
ShortForge <s.n.psaradellis@gmail.com>
2025-11-11 18:54:22 -0600

    build issues

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,9 +1,15 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
     domains: ['shortforge-backend-production.up.railway.app', 'localhost'],
+  },
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    return config;
   },
 }
 
